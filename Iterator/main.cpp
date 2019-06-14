899a377bbb10cca650f789a02f8c613f6646291d
duxingtianxia <guweicao@sina.com>
2019-06-14 22:28:16 +0800

    add iterator

---
+++ Iterator/main.cpp
@@ -0,0 +1,16 @@
+#include "Iterator.h"
+#include "Aggregate.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	Aggregate* ag = new ConcreteAggregate();
+	Iterator* it = new ConcreteIterator(ag);
+	for (; !(it->IsDone()); it->Next())
+	{
+		cout << it->CurrenItem() << endl;
+	}
+	system("pause");
+	return 0;
+}
